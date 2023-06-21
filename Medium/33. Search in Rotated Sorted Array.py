8085184b7c548d3f9cf7416d612c365e229dd91b
Linh T Nguyen <92663471+linh010604@users.noreply.github.com>
2023-06-21 00:22:40 -0400

    Create 33. Search in Rotated Sorted Array.py

---
+++ Medium/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target not in nums :
+            return -1
+        return nums.index(target)
