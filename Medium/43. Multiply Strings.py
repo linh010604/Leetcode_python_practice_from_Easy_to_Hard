39ec192fc27af423fafd3a59c27d88f74231aa3f
Linh T Nguyen <92663471+linh010604@users.noreply.github.com>
2023-06-26 16:29:06 -0400

    Create 43. Multiply Strings.py

---
+++ Medium/43. Multiply Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num11 = int(num1)
+        num22 = int(num2)
+
+        return str(num11*num22)
