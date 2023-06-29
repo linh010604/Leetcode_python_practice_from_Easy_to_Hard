f7bd31494247ed944d292a2e5deace8552462a59
Linh T Nguyen <92663471+linh010604@users.noreply.github.com>
2023-06-29 16:07:11 -0400

    Create 796. Rotate String.py

---
+++ Easy/796. Rotate String.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        
+        goal += goal
+
+        if s in goal:
+            return True
+        return False
