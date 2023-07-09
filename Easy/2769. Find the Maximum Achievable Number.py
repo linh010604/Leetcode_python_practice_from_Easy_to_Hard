bb7e4099e6c8139691f60334d37ccf39c0a85c84
Linh T Nguyen <92663471+linh010604@users.noreply.github.com>
2023-07-09 00:07:33 -0400

    Create 2769. Find the Maximum Achievable Number.py

---
+++ Easy/2769. Find the Maximum Achievable Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        
+        return num + 2*t
+        
